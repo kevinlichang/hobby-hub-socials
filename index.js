00b78f608a421c4a66464ff4d889bf17d268d419
changkev <38297595+kevinlichang@users.noreply.github.com>
2021-09-18 22:02:12 -0400

    setting up queries for registering users

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
 const { ApolloServer } = require('apollo-server');
-const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 
-const typeDefs = gql`
-  type Query{
-    sayHi: String!
-  }
-`
+require('dotenv').config();
 
-const resolvers = {
-  Query: {
-    sayHi: () => 'Hello World!!!!!!'
-  }
-}
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+
+
 
 const server = new ApolloServer({
   typeDefs,
   resolvers
 });
 
-mongoose.connect()
-
-server.listen({ port: 5000 })
-  .then(res => {
-    console.log(`Server running at ${res.url}`);
-  });
-
+mongoose.connect(process.env.DB_URI,
+  { useNewUrlParser: true }
+).then(() => {
+  console.log('MongoDB successfully connected');
+  return server.listen({ port: 5000 });
+}).then((res) => {
+  console.log(`Server running at ${res.url}`)
+}).catch((error) => {
+  console.log("Unable to connect to MongoDB");
+  console.error(error);
+});
